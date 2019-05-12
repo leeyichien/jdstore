07ddbfb896f4262098621e05acbc2407b6672cc8
leeyichien <tdem.tdem@me.com>
2019-05-12 10:41:14 +0800

    move sqlite3 to dev group & add pg to production group

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,8 +5,8 @@ include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
- storage :fog
-  # storage :fog
+    storage :fog
+
 
 
   # Override the directory where uploaded files will be stored.
